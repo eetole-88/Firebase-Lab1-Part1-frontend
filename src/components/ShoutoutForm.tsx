307431c10d6607cc14668c1fe47e7b8ca56ff585
Emilie Tole <eetole@gmail.com>
2021-10-01 09:28:44 -0400

    css changes

--- src/components/ShoutoutForm.tsx
+++ src/components/ShoutoutForm.tsx
@@ -1,5 +1,6 @@
 import { FormEvent, useState } from "react";
 import Shoutout from "..//models/shoutout-api-model";
+import "./ShoutoutForm.css";
 
 interface Props {
   onSubmit: (item: Shoutout) => void;
@@ -24,39 +25,46 @@ const ShoutoutForm = ({ onSubmit }: Props) => {
   }
 
   return (
-    <form className="ShoutoutForm" onSubmit={handleSubmit}>
-      <h2>Leave a Shout Out</h2>
-      <p>
-        <label htmlFor="ShoutoutForm__to">To</label>
-        <input
-          type="text"
-          id="ShoutoutForm__to"
-          value={to}
-          onChange={(e) => setTo(e.target.value)}
-        />
-      </p>
-      <p>
-        <label htmlFor="ShoutoutForm__from">From</label>
-        <input
-          type="text"
-          id="ShoutoutForm__from"
-          value={from}
-          onChange={(e) => setFrom(e.target.value)}
-        />
-      </p>
-      <p>
-        <label htmlFor="ShoutoutForm__message">Shout Out</label>
-        <input
-          type="text"
-          id="ShoutoutForm__message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-      </p>
-      <p>
-        <button type="submit">Submit Shout Out!</button>
-      </p>
-    </form>
+    <section>
+      <form className="ShoutoutForm" onSubmit={handleSubmit}>
+        <h2>Leave a Shout Out</h2>
+        <p>
+          <label htmlFor="ShoutoutForm__to">To</label>
+        </p>
+        <p>
+          <input
+            type="text"
+            id="ShoutoutForm__to"
+            value={to}
+            onChange={(e) => setTo(e.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor="ShoutoutForm__from">From</label>
+        </p>
+        <p>
+          <input
+            type="text"
+            id="ShoutoutForm__from"
+            value={from}
+            onChange={(e) => setFrom(e.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor="ShoutoutForm__message">Shout Out</label>
+        </p>
+        <p>
+          <textarea
+            id="ShoutoutForm__message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </p>
+        <p>
+          <button type="submit">Submit Shout Out!</button>
+        </p>
+      </form>
+    </section>
   );
 };
 
